7309e6c3a0d8f22b85f2e315e5071cb8b66d7fd1
MuhammetBayrak <msbayrak08@gmail.com>
2026-05-17 20:03:26 +0300

    EntityLayer - All entities added

---
+++ EntityLayer/Abstract/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Abstract
+{
+    public abstract class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
