5a62aca4eb00b25d0ad453dede39feed81949986
MuhammetBayrak <msbayrak08@gmail.com>
2026-05-18 22:43:05 +0300

    feature: admin layout and dashboard added

---
+++ PersonalSite.UI/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalSite.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Dashboard";
+            return View();
+        }
+    }
+}
