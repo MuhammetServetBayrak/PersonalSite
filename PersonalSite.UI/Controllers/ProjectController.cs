70e577e34b6b044d916ffa93e2e50551b7a88395
MuhammetBayrak <msbayrak08@gmail.com>
2026-05-19 12:12:28 +0300

    feature: admin project CRUD views added

---
+++ PersonalSite.UI/Controllers/ProjectController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalSite.UI.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Projeler";
+            var projects = _projectService.GetAll();
+            return View(projects);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "Yeni Proje";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            project.CreatedDate = DateTime.UtcNow;
+            project.IsActive = true;
+            _projectService.Add(project);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ViewData["Title"] = "Projeyi Düzenle";
+            var project = _projectService.GetById(id);
+            return View(project);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Project project)
+        {
+            _projectService.Update(project);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var project = _projectService.GetById(id);
+            _projectService.Delete(project);
+            return RedirectToAction("Index");
+        }
+    }
+}
