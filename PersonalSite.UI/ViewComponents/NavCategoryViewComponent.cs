92e59251403425f3b8f6b3900df163e38a9b03a8
MuhammetBayrak <msbayrak08@gmail.com>
2026-05-24 06:35:03 +0300

    feature: navbar links updated

---
+++ PersonalSite.UI/ViewComponents/NavCategoryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalSite.UI.ViewComponents
+{
+    public class NavCategoryViewComponent : ViewComponent
+    {
+        private readonly IBlogCategoryService _blogCategoryService;
+
+        public NavCategoryViewComponent(IBlogCategoryService blogCategoryService)
+        {
+            _blogCategoryService = blogCategoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _blogCategoryService.GetAll().Where(x => x.IsActive).ToList();
+            return View(categories);
+        }
+    }
+}
